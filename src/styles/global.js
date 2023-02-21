4d9e46b1ce4062770ad18aef14c4ad774e6c7d77
teste-agua-edu <gui.2001@hotmail.com>
2023-02-21 16:53:40 -0300

    feat-style: styled

--- src/styles/global.js
+++ src/styles/global.js
@@ -2,8 +2,8 @@ import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
   body {
-   background: #222;
-   font-family: sans-serif;
-   color: #fff;
+    background: #222;
+    color: #fff;
+    font-family: sans-serif;
   }
 `;
