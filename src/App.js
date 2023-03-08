5135e4264fa06680577f59bec8014cf51506965a
teste-agua-edu <gui.2001@hotmail.com>
2023-03-08 20:17:24 -0300

    feat-adicao de novos metodos usando classes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { ThemeProvider as StyleThemeProvider } from "styled-components";
 import { ThemeProvider, ThemeContext } from "./context/ThemeContext";
 import Layout from "./components/Layout";
 import GlobalStyle from "./styles/global";
 import themes from "./styles/themes";
 
+// componentDidMount mesma coisa do useEffect sem dependencias, só vai renderizar uma única vez
+// componentDidUpdate = chamado depois do metodo render, e executado toda vez que tem uma atualização de estado na aplicação
+
 class App extends React.Component {
+  state = {
+    changed: false,
+  };
+
+  // componentDidMount - executado na renderização da tela
+  componentDidMount() {
+    console.log("componentDidMount executed");
+  }
+
+  // componentDidUpdate - monitora as alterações, pegando as propriedades anteriores e as atuais, podendo fazer a comparação das alterações
+  componentDidUpdate(prevProps, prevState) {
+    console.log({
+      currentState: this.state,
+      prevState,
+      prevProps,
+    });
+  }
+
+  // componentDidCatch - serve para capturar os erros que ocorrem dentro do método render
+  componentDidCatch(error, info) {
+    console.log(error);
+  }
+
+  // shouldComponentUpdate - semelhante ao DidUpdate, é o único do lifecycle que deve retornar um valor booleano - O SHOULD COMPONENT É RENDERIZADO ANTES DA TELA SER MONTADA
+  // nextProps - valores que vai receber
+  // nextState - valor que o usuário está tentando atualizar
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log({
+      currentState: this.state,
+      nextProps,
+      nextState,
+    });
+
+    return true;
+    // retornamos true se quisermos renderizar, se for falso impedimos a atualização do componente
+  }
+
+  // componentWillUnmount - componente será executado antes do componente sair da tela
+  componentWillUnmount() {}
+
   render() {
+    console.log("rendered");
     return (
       <ThemeProvider>
         <ThemeContext.Consumer>
@@ -62,3 +106,5 @@ export default App;
 //     </ContextTheme.Provider>
 //   );
 // }
+
+// RENDER PROPS = passando as propriedades através das props da context renderizando por uma funcao
--- src/context/ThemeContext.js
+++ src/context/ThemeContext.js
@@ -1,17 +1,54 @@
-import React, { createContext, useState } from "react";
+import React, { createContext } from "react";
 
 export const ThemeContext = createContext();
 
-export function ThemeProvider({ children }) {
-  const [theme, setTheme] = useState("dark");
+export class ThemeProvider extends React.Component {
+  //constructor - executado quando a classe é instanciada
+  constructor(props) {
+    // super - executamos o método construtor da classe que estamos herdandno
+    super(props);
 
-  function handleToggleTheme() {
-    setTheme(prevState => prevState === "dark" ? "light" : "dark");
+    let theme = "dark";
+
+    try {
+      theme = JSON.parse(localStorage.getItem("theme"));
+    } catch (err) {
+      console.log(err);
+    }
+
+    this.state = {
+      theme,
+    };
   }
 
-  return (
-    <ThemeContext.Provider value={{ theme, handleToggleTheme }}>
-      {children}
-    </ThemeContext.Provider>
-  );
+  // usamos para monitorar quando uma variável de classe mudou - semelhante á dependencia do useEffect(() => {},[theme])
+  componentDidUpdate(prevPrrops, prevState) {
+    if (prevState.theme !== this.state.theme) {
+      localStorage.setItem("theme", JSON.stringify(this.state.theme));
+    }
+  }
+
+  handleToggleTheme = () => {
+    this.setState(
+      (prevState) => ({
+        theme: prevState.theme === "dark" ? "light" : "dark",
+      }),
+      () => {
+        console.log(this.state);
+      }
+    );
+  };
+
+  render() {
+    return (
+      <ThemeContext.Provider
+        value={{
+          theme: this.state.theme,
+          handleToggleTheme: this.handleToggleTheme,
+        }}
+      >
+        {this.props.children}
+      </ThemeContext.Provider>
+    );
+  }
 }
